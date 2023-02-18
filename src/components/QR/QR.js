da69b9253e76e5cf0090e6f9b29b6f09f4efca7a
Allan <allankong176@gmail.com>
2023-02-18 16:23:55 -0700

    added some more details for qr code

--- src/components/QR/QR.js
+++ src/components/QR/QR.js
@@ -8,9 +8,9 @@ export function QR({id}){
    //🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒
 
    const [data, setData] = useState([{}]);
+   const [data1, setData1] = useState([{}]);
 
    useEffect(() => {
-      let id=1
       const interval = setInterval(() => {
 
          fetch(`/getQR?id=1`)
@@ -19,14 +19,32 @@ export function QR({id}){
           setData(data);
          });
 
+         fetch(`/getName?id=1`)
+        .then((res) => res.json())
+        .then((data1) => {
+         console.log(data1);
+         setData1(data1);
+         });
+
       }, 1000);
         return () => clearInterval(interval);
     }, []);
    return (
+      
 
-      <div>
+      <div style={{ 
+         margin: "0",
+         position: "absolute",
+         top: "50%",
+         left: "50%",
+         transform: "translate(-50%, -50%)",
+         }}>
+         
+         <h1 style={{textAlign: "center"}}>{data1}</h1>
+         
          <QRCodeSVG value={data}
          size={500}/>
+
       </div>
    );
 }
