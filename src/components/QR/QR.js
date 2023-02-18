395458167ae13a5760ab6eb989415ee3efe6e0a9
Allan <allankong176@gmail.com>
2023-02-18 15:39:04 -0700

    JavaScript gets QR string from frontend

--- src/components/QR/QR.js
+++ src/components/QR/QR.js
@@ -1,7 +1,31 @@
-export function QR(){
+import React, { useState, useEffect } from "react";
+
+export function QR({id}){
+   //🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒
+   //🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒
+   //🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒
+   //🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒🐒
+
+   const [data, setData] = useState([{}]);
+
+   useEffect(() => {
+      let id=1
+      const interval = setInterval(() => {
+
+         fetch(`/getQR?id=1`)
+        .then((res) => res.json())
+        .then((data) => {
+          setData(data);
+          console.log(data);
+        });
+
+      }, 1000);
+        return () => clearInterval(interval);
+    }, []);
    return (
+
       <div>
          hi
       </div>
-   )
+   );
 }
