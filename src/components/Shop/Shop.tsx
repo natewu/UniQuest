acdcfce70871858a932fadb6ee193a8bf5753c17
natewu <36091727+natewu@users.noreply.github.com>
2023-02-19 02:30:27 -0700

    shop

---
+++ src/components/Shop/Shop.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Shop() {
+   return (
+      <div className="Shop">
+         <h1>Shop</h1>
+         <p>Coming soon...</p>
+         
+      </div>
+   )
+}
+
+export default Shop
