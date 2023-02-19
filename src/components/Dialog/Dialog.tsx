9578ddc802445d22dce0d0a649e5994846cab5c6
natewu <36091727+natewu@users.noreply.github.com>
2023-02-18 18:44:29 -0700

    dialog box

---
+++ src/components/Dialog/Dialog.tsx
@@ -0,0 +1,13 @@
+import styles from './Dialog.module.scss'
+
+export function Dialog({open, onClose, children}: {open: boolean, onClose: () => void, children: React.ReactNode}){
+   return (
+      <div className={styles.Dialog}>
+         <div className={styles.Dialog__container}>
+            <div className={styles.Dialog__content}>
+               {children}
+            </div>
+         </div>
+      </div>
+   )
+}
