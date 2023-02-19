92486c0899fa1dcde72723e9ecc0bc641c22de9e
natewu <36091727+natewu@users.noreply.github.com>
2023-02-19 11:19:46 -0700

    profile

--- src/components/Status/Status.js
+++ src/components/Status/Status.js
@@ -16,41 +16,46 @@ export function Status(){
             
             <Profile data={{points:20, name:"kongaroo", profile:"https://i.pinimg.com/280x280_RS/e4/86/86/e4868681b6e2cc4ce83c3b9a5d010b6c.jpg"}}/>
 
-            <div className={styles.egg}></div>
+            {/* <div className={styles.egg}></div> */}
             <div class={styles.main}>
+                <div>
+                    <h3 className={styles.header}>Today</h3>
 
-            <h3 className={styles.header}>Today</h3>
+                    <div className={styles.container}>
+                        <div className={styles.info} >
+                            <p className={styles.values}>Rewards</p>
+                            <p className={styles.values} >Points</p>
+                            <p className={styles.values}>Codes</p>
+                        </div>
+                        <div className={styles.info2}>
+                            <p className={styles.values}>$2</p>
+                            <p className={styles.values}>100</p>
+                            <p className={styles.values}>6</p>
 
-                <div className={styles.container}>
-                    <ul className={styles.firstValuesList} >
-                        <li className={styles.values}>Rewards</li>
-                        <li className={styles.values} >Points</li>
-                        <li className={styles.values}>Codes</li>
-                    </ul>
-                    <ul className={styles.valuesList}>
-                        <li className={styles.values}>$2</li>
-                        <li className={styles.values}>100</li>
-                        <li className={styles.values}>6</li>
-
-                    </ul>
+                        </div>
+                    </div>    
                 </div>
+                
 
-                <h3 className={styles.header}>Lifetime</h3>
+                <div>
+                    <h3 className={styles.header}>Lifetime</h3>
 
 
-                <div className={styles.container}>
-                    <ul className={styles.firstValuesList}>
-                        {/* <h3 className={styles.header}>Lifetime</h3> */}
-                        <li className={styles.values}>Rewards</li>
-                        <li className={styles.values}>Points</li>
-                        <li className={styles.values}>Codes</li>
-                    </ul>
-                    <ul className={styles.valuesList}>
-                        <li className={styles.values}>$20</li>
-                        <li className={styles.values}>840</li>
-                        <li className={styles.values}>42</li>
-                    </ul>
+                    <div className={styles.container}>
+                        <div className={styles.info}>
+                            {/* <h3 className={styles.header}>pfetime</h3> */}
+                            <p className={styles.values}>Rewards</p>
+                            <p className={styles.values}>Points</p>
+                            <p className={styles.values}>Codes</p>
+                        </div>
+                        <div className={styles.info2}>
+                            <p className={styles.values}>$20</p>
+                            <p className={styles.values}>840</p>
+                            <p className={styles.values}>42</p>
+                        </div>
+                    </div>
                 </div>
+                
                 
         
             </div>
