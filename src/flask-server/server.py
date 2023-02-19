711af48f9cd0c67adcd6bb818fa1d02b8f8fa324
AnaCoda <anacod@zoho.com>
2023-02-19 08:56:00 -0700

    Send correct amount of points

--- src/flask-server/server.py
+++ src/flask-server/server.py
@@ -68,7 +68,7 @@ def validQR():
 				user.points += screen.points
 				user.screens.append(screen)	
 				db.session.commit()
-				return {'valid': "Good job!", "points": "20", "description":screen.description, "imageURL":screen.imageURL}
+				return {'valid': "Good job!", "points": str(screen.points), "description":screen.description, "imageURL":screen.imageURL}
 			else:
 				return {'valid': "You already scanned this :)"}
 	return {'valid': ""}
